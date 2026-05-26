68506c5f82e5a3bdaf137f9ccdb07fef766536ce
ardasaracgm <ardasaracgm@gmail.com>
2026-05-26 15:52:59 +0300

    Delete app/ferry directory

--- app/ferry/layout.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { BookingProvider } from '@/lib/booking-context'
-
-export default function FerryLayout({ children }: { children: React.ReactNode }) {
-  return <BookingProvider>{children}</BookingProvider>
-}
